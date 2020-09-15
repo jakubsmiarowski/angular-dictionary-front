fb31b1551aaa5f63ee55dc7747c522e3d3133a66
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2020-09-15 18:19:14 +0200

    add form for view dictonaries

--- src/app/view-dictionary/view-dictionary.component.ts
+++ src/app/view-dictionary/view-dictionary.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-view-dictionary',
   templateUrl: './view-dictionary.component.html',
-  styleUrls: ['./view-dictionary.component.css']
+  styleUrls: ['./view-dictionary.component.css'],
 })
 export class ViewDictionaryComponent implements OnInit {
+  @ViewChild('f', { static: false }) signupForm: NgForm;
+  submitted = false;
+  dictName = '';
+  dictType = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit(form: NgForm) {
+    console.log(form);
+    this.submitted = true;
+    this.dictName = this.signupForm.value.userData.dictionary;
+    this.dictType = this.signupForm.value.type;
+    console.log(this.dictName);
+    console.log(this.dictType);
   }
-
 }
